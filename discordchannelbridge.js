9a4f890ff2a42dd6fdde9bd42911da012c977520
QDave <davidav08@gmail.com>
2023-11-12 19:11:12 +0100

    Update discordchannelbridge.js

--- discordchannelbridge.js
+++ discordchannelbridge.js
@@ -1,19 +1,36 @@
+const fs = require('fs');
 const { Client } = require('discord.js-selfbot-v13');
 const { Client: BotClient, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-
 const guildsChannelsMappings = [
+  
     {
-        sourceGuildId: 'source_server_id_here',
-        sourceChannelId: 'source_channel_id_here',
-        targetGuildId: 'target_server_id_here',
-        targetChannelId: 'target_channel_id_here'
-    }, 
-
-    // add multiple if needed
+        sourceGuildId: '',
+        sourceChannelId: '',
+        targetGuildId: '',
+        targetChannelId: ''
+    },
+    
+// add multiple if needed
+                
 ];
-
-const roleWhitelist = ['role_id_1','role_id_2','role_id_3']; // which role ids should be shown
-
+const sentMessagesPath = './sentMessages.txt';
+const sentMessages = new Set();
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const log = (message) => {
+    console.log(`${new Date().toISOString()} - ${message}`);
+};
+const loadSentMessages = () => {
+    if (fs.existsSync(sentMessagesPath)) {
+        const fileContent = fs.readFileSync(sentMessagesPath, 'utf-8');
+        fileContent.split('\n').forEach(id => {
+            if (id) sentMessages.add(id);
+        });
+    }
+};
+const saveMessageId = (messageId) => {
+    fs.appendFileSync(sentMessagesPath, `${messageId}\n`, 'utf-8');
+    sentMessages.add(messageId);
+};
 const selfbot = new Client({ checkUpdate: false });
 const botClient = new BotClient({
     intents: [
@@ -22,55 +39,80 @@ const botClient = new BotClient({
         GatewayIntentBits.MessageContent,
     ]
 });
-
-selfbot.on('ready', () => {
-    console.log(`Selfbot logged in as ${selfbot.user.tag}!`);
+botClient.login('Bot_token_here');
+selfbot.login('Selfbot_token_here');
+selfbot.on('error', (error) => {
+    log(`Error from selfbot client: ${error}`);
 });
 
-selfbot.on('messageCreate', async message => {
-    const config = guildsChannelsMappings.find(c => c.sourceGuildId === message.guild.id && c.sourceChannelId === message.channel.id);
-    if (config) {
-        const displayName = message.member ? message.member.displayName : message.author.username;
-        const avatarURL = message.author.displayAvatarURL();
+let messageCheckInterval;
 
-        const embed = new EmbedBuilder()
-            .setAuthor({ name: `(${displayName}) [${message.author.tag}]`, iconURL: avatarURL })
-            .setFooter({ text: `Server: ${message.guild.name} | Channel: ${message.channel.name}` });
-
-        const roles = message.member.roles.cache
-            .filter(role => roleWhitelist.includes(role.id))
-            .map(role => `\`${role.name}\``)
-            .join(' | ');
-
-        embed.addFields({ name: '*Roles*', value: roles.length > 0 ? roles : '*No Roles*' });
-
-        if (message.content) {
-            embed.setDescription(`> **${message.content}**`);
-        }
-        
-
-        const attachments = message.attachments.filter(attachment => attachment.contentType && attachment.contentType.startsWith('image/'));
-        if (attachments.size > 0) {
-            embed.setImage(attachments.first().url);
-        } else {
-            const imageUrls = message.content.match(/\bhttps?:\/\/\S+(?:png|jpg|jpeg|gif)\b/gi);
-            if (imageUrls && imageUrls.length > 0) {
-                embed.setImage(imageUrls[0]);
-            }
-        }
-        const targetGuild = botClient.guilds.cache.get(config.targetGuildId);
-        if (targetGuild) {
-            const targetChannel = targetGuild.channels.cache.get(config.targetChannelId);
-            if (targetChannel) {
-                await targetChannel.send({ embeds: [embed] });
-            }
-        }
-    }
+selfbot.on('ready', () => {
+    log(`Selfbot client logged in as ${selfbot.user.tag}!`);
+    loadSentMessages();
+    checkForNewMessages();
 });
 
 botClient.on('ready', () => {
-    console.log(`Bot logged in as ${botClient.user.tag}!`);
+    log(`Bot client logged in as ${botClient.user.tag}!`);
 });
 
-selfbot.login('SELFBOT_TOKEN_HERE');
-botClient.login('BOT_TOKEN_HERE');
+const logError = (error) => {
+
+  if (error instanceof Error) {
+      log(`Error message: ${error.message}`);
+      if ('code' in error) log(`Error code: ${error.code}`);
+      if ('httpStatus' in error) log(`HTTP status: ${error.httpStatus}`);
+      if ('requestData' in error) log(`Request data: ${JSON.stringify(error.requestData, null, 2)}`);
+  } else {
+      log(`Unknown error type: ${JSON.stringify(error, null, 2)}`);
+  }
+};
+
+let isCheckingMessages = false;
+
+const checkForNewMessages = async () => {
+    if (isCheckingMessages) {
+        return;
+    }
+
+    isCheckingMessages = true;
+    await sleep(3000);
+
+    for (const [index, config] of guildsChannelsMappings.entries()) {
+
+        try {
+            const sourceGuild = await selfbot.guilds.fetch(config.sourceGuildId);
+            const sourceChannel = await sourceGuild.channels.fetch(config.sourceChannelId);
+            const messages = await sourceChannel.messages.fetch({ limit: 20 });
+
+            const newMessages = messages.filter(message => !sentMessages.has(message.id) && !message.author.bot);
+            for (const [messageId, message] of newMessages) {
+                const displayName = message.member ? message.member.displayName : message.author.username;
+                
+                const embed = new EmbedBuilder()
+                    .setAuthor({ name: `${displayName} [${message.author.tag}]`, iconURL: message.author.displayAvatarURL() })
+                    .setDescription(message.content)
+                    .setTimestamp(message.createdTimestamp);
+
+                if (message.attachments.size > 0) {
+                    const imageAttachment = message.attachments.find(attachment => attachment.contentType?.startsWith('image/'));
+                    if (imageAttachment) {
+                        embed.setImage(imageAttachment.proxyURL);
+                    }
+                }
+
+                const targetGuild = botClient.guilds.cache.get(config.targetGuildId);
+                const targetChannel = targetGuild.channels.cache.get(config.targetChannelId);
+                await targetChannel.send({ embeds: [embed] });
+                saveMessageId(messageId);
+                await sleep(3000);
+            }
+        } catch (error) {
+            logError(error);
+        }
+        await sleep(30000);
+    }
+    isCheckingMessages = false;
+    setTimeout(checkForNewMessages, 600000);
+};
